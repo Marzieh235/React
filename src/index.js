ff8622ff57bdc261b8bd99531bbd4c56d35a9ce1
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-17 01:49:26 +0330

    Getting to know and working with jsx

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+window.element = React.createElement('h1',{className:'style'},'hello world')
+
+const root = ReactDOM.createRoot(window.element , document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />

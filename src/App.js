edfd5eeff4c25da0766fd4a4aa6897f3e7fc832a
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-17 21:40:54 +0330

    Dynamic styles

--- src/App.js
+++ src/App.js
@@ -1,66 +1,87 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React , { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
 
 
 class App extends React.Component {
   state = {
-    articles: [
-      { id: 1, title: 'article 1', body: 'this is article 1', active: 1 },
-      { id: 2, title: 'article 2', body: 'this is article 2', active: 0 },
-      { id: 3, title: 'article 3', body: 'this is article 3', active: 1 },
+    articles : [
+      { id : 1 , title : 'article 1' , body : 'this is article 1' , active: 1},
+      { id : 2 , title : 'article 2' , body : 'this is article 2' , active: 0},
+      { id : 3 , title : 'article 3' , body : 'this is article 3' , active: 1},
     ],
-    title: 'this is test ',
-    loading: false,
+    title : 'this is roocket',
+    loading : false,
+    btnStyle : { 
+      backgroundColor : 'rgba(0,0,0,.8)',
+      padding : '10px 25px' ,
+      color : 'white' ,
+      border : 'none' ,
+      borderRadius : '3px'
+    }
   }
 
   loadMore = () => {
-    this.setState({ loading: true })
-    setTimeout(() => {
-      let articles = [
-        { id: 4, title: 'article 4', body: 'this is article 4', active: 1 },
-        { id: 5, title: 'article 5', body: 'this is article 5', active: 0 },
-        { id: 6, title: 'article 6', body: 'this is article 6', active: 1 },
-      ]
-
-
-      this.setState((prevState => {
-        return {
-          articles: [...prevState.articles, ...articles],
-          loading: false
+      this.setState({ loading : true })
+      setTimeout(() => {
+        let articles = [
+          { id : 4 , title : 'article 4' , body : 'this is article 4' , active: 1},
+          { id : 5 , title : 'article 5' , body : 'this is article 5' , active: 0},
+          { id : 6 , title : 'article 6' , body : 'this is article 6' , active: 1},
+        ]
+  
+  
+        this.setState(prevState => {
+          return {
+            articles : [...prevState.articles , ...articles],
+            loading : false
+          }
+        })
+      }, 3000);
+  }
+  
+  mosueEnter = (e) => {
+    console.log({
+      ...this.state.btnStyle,
+      backgroundColor : 'rgba(0,0,0,.5)'
+    })
+    this.setState(prevState => {
+      return {
+        btnStyle : {
+          ...prevState.btnStyle,
+          backgroundColor : 'rgba(0,0,0,.5)'
         }
-      }))
-    }, 3000);
+      }
+    })
+  }
 
+  mosueLeave = (e) => {
+    this.setState(prevState => {
+      return {
+        btnStyle : {
+          ...prevState.btnStyle,
+          backgroundColor : 'rgba(0,0,0,.8)'
+        }
+      }
+    })
 
   }
 
   render() {
-    console.log(this.state);
-    let articleList = this.state.articles.map((article, index) => article.active ? <Card key={index} title={article.title} body={article.body} /> : null)
-
-    const BtnStyle = {
-      backgroundColor: '#555',
-      padding: '10px 25px',
-      border: 'none',
-      color: 'white',
-      borderRadius: '3px'
-    }
+    let articleList = this.state.articles.map((article , index) => article.active ? <Card key={index} title={article.title} body={article.body} /> : null)
 
     return (
       <div className="app">
-        {articleList}
-        {this.state.loading
-          ? <div>loading ...</div>
-          : null}
-        <button style={BtnStyle} 
-        onClick={this.loadMore} 
-        onMouseEnter={(e)=>} 
-        onMouseLeave={}
-        >
-          load more
-          </button>
+        { articleList  }
+        { this.state.loading 
+             ? <div>Loading ...</div> : null }
+        <button 
+          style={this.state.btnStyle} onClick={this.loadMore} 
+          onMouseEnter={this.mosueEnter}
+          onMouseLeave={this.mosueLeave} 
+
+          >load more</button>
       </div>
     );
   }

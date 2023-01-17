8dec9acc57d97249ce365266a7d49963ab23c43f
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-17 15:35:17 +0330

    Usestates in functional components

--- src/App.js
+++ src/App.js
@@ -1,80 +1,67 @@
+import React , { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Card from './Card';
-import React from 'react';
 
-
-
-
-class App extends React.Component {
-  state = {
-    article : [
-      {id : 1 , title : 'article 1' , body : 'this is article 1'},
-      {id : 2 , title : 'article 2' , body : 'this is article 2'},
-      {id : 3 , title : 'article 3' , body : 'this is article 3'},
-
+function App() {
+  const [stateArticle , setArticleState] = useState({
+    articles : [
+      { id : 1 , title : 'article 1' , body : 'this is article 1'},
+      { id : 2 , title : 'article 2' , body : 'this is article 2'},
+      { id : 3 , title : 'article 3' , body : 'this is article 3'},
     ]
-  }
-  
-  constructor(){
-    super()
+  })
+
     setTimeout(() => {
-      this.setState({
-        article : [
-          {id : 4 , title : 'article 4' , body : 'this is article 4'},
-          {id : 5 , title : 'article 5' , body : 'this is article 5'},
-          {id : 6 , title : 'article 6' , body : 'this is article 6'},
-    
+      setArticleState({
+        articles : [
+          { id : 4 , title : 'article 4' , body : 'this is article 4'},
+          { id : 5 , title : 'article 5' , body : 'this is article 5'},
+          { id : 6 , title : 'article 6' , body : 'this is article 6'},
         ]
       })
-    }, 3000);
-  }
+    }, 2000);
 
-
-
-
-
-  render() {
-    return (
-      <div className='app'>
-        <Card title={this.state.article[0].title} body={this.state.article[0].body} />
-        <Card title={this.state.article[1].title} body={this.state.article[1].body} />
-        <Card title={this.state.article[2].title} body={this.state.article[2].body} />
-
-      </div>
-    )
-
-
-  }
+  return (
+    <div className="app">
+      <Card title={stateArticle.articles[0].title} body={stateArticle.articles[0].body}/>
+      <Card title={stateArticle.articles[1].title} body={stateArticle.articles[1].body}/>
+      <Card title={stateArticle.articles[2].title} body={stateArticle.articles[2].body}/>
+  </div>
+  );
 }
 
-// function App() {
-//   return(
-// <div className='app'>
-//   <Card title = "Article 1" body ="this is Article 1"/>
-//   <Card title = "Article 2" body ="this is Article 2"/>
-//   <Card title = "Article 3" body ="this is Article 3"/>
+// class App extends React.Component {
+//   state = {
+//     articles : [
+//       { id : 1 , title : 'article 1' , body : 'this is article 1'},
+//       { id : 2 , title : 'article 2' , body : 'this is article 2'},
+//       { id : 3 , title : 'article 3' , body : 'this is article 3'},
+//     ]
+//   }
 
-// </div>
-//   )
+//   constructor() {
+//     super()
+//     setTimeout(() => {
+//       this.setState({
+//         articles : [
+//           { id : 4 , title : 'article 4' , body : 'this is article 4'},
+//           { id : 5 , title : 'article 5' , body : 'this is article 5'},
+//           { id : 6 , title : 'article 6' , body : 'this is article 6'},
+//         ]
+//       })
+//     }, 2000);
+//   }
+
+//   render() {
+//     return (
+//       <div className="app">
+//           <Card title={this.state.articles[0].title} body={this.state.articles[0].body}/>
+//           <Card title={this.state.articles[1].title} body={this.state.articles[1].body}/>
+//           <Card title={this.state.articles[2].title} body={this.state.articles[2].body}/>
+//       </div>
+//     )
+//   }
 // }
-// return (
-//   <div className="App">
-//     <header className="App-header">
-//       <img src={logo} className="App-logo" alt="logo" />
-//       <p>
-//         Edit <code>src/App.js</code> and save to reload.
-//       </p>
-//       <a
-//         className="App-link"
-//         href="https://reactjs.org"
-//         target="_blank"
-//         rel="noopener noreferrer"
-//       >
-//         Learn React
-//       </a>
-//     </header>
-//   </div>
-// );
 
 export default App;

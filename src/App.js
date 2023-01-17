743acd681c7d7eb14a58d3d77f93d8c5f8bb3ce4
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-17 21:15:26 +0330

    Using linear styles

--- src/App.js
+++ src/App.js
@@ -7,28 +7,28 @@ import Card from './components/Card';
 class App extends React.Component {
   state = {
     articles: [
-      { id: 1, title: 'article 1', body: 'this is article 1' ,active :1 },
-      { id: 2, title: 'article 2', body: 'this is article 2' ,active :0},
-      { id: 3, title: 'article 3', body: 'this is article 3' ,active :1},
+      { id: 1, title: 'article 1', body: 'this is article 1', active: 1 },
+      { id: 2, title: 'article 2', body: 'this is article 2', active: 0 },
+      { id: 3, title: 'article 3', body: 'this is article 3', active: 1 },
     ],
     title: 'this is test ',
     loading: false,
   }
 
   loadMore = () => {
-    this.setState({loading : true})
+    this.setState({ loading: true })
     setTimeout(() => {
       let articles = [
-        { id: 4, title: 'article 4', body: 'this is article 4' ,active :1},
-        { id: 5, title: 'article 5', body: 'this is article 5' ,active :0},
-        { id: 6, title: 'article 6', body: 'this is article 6' ,active :1},
+        { id: 4, title: 'article 4', body: 'this is article 4', active: 1 },
+        { id: 5, title: 'article 5', body: 'this is article 5', active: 0 },
+        { id: 6, title: 'article 6', body: 'this is article 6', active: 1 },
       ]
 
 
       this.setState((prevState => {
         return {
           articles: [...prevState.articles, ...articles],
-          loading : false
+          loading: false
         }
       }))
     }, 3000);
@@ -40,13 +40,21 @@ class App extends React.Component {
     console.log(this.state);
     let articleList = this.state.articles.map((article, index) => article.active ? <Card key={index} title={article.title} body={article.body} /> : null)
 
+    const BtnStyle = {
+      backgroundColor: '#555',
+      padding: '10px 25px',
+      border: 'none',
+      color: 'white',
+      borderRadius: '3px'
+    }
+
     return (
       <div className="app">
         {articleList}
-        { this.state.loading
-        ? <div>loading ...</div>
-        : null }
-        <button onClick={this.loadMore}>load more</button>
+        {this.state.loading
+          ? <div>loading ...</div>
+          : null}
+        <button style={BtnStyle} onClick={this.loadMore}>load more</button>
       </div>
     );
   }

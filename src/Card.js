2f547a207ca3e0ece0812d31954e4a91c000eee5
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-17 14:15:28 +0330

    Class based components

--- src/Card.js
+++ src/Card.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 
-
-
-
-
-
-
-function Card(props){
-return (
-    <div className="card">
-        <header className="card-header">
-            <h2>{props.title}</h2>
-        </header>
-        <section className="card-body">
-            <p>{props.body}</p>
-        </section>
-        <footer>
-            <p>Card footer</p>
-        </footer>
-    </div>
-)
+class Card extends Component {
+    render() {
+        let {title , body} = this.props;
+        return (
+            <div className="card">
+                <header className="card-header">
+                    <h2>{title}</h2>
+                </header>
+                <section className="card-body">
+                    <p>{body}</p>
+                </section>
+                <footer>
+                    <p>Card footer</p>
+                </footer>
+            </div>
+        )
+    }
 }
+
+
+
+
+
+
+// function Card(props){
+// return (
+//     <div className="card">
+//         <header className="card-header">
+//             <h2>{props.title}</h2>
+//         </header>
+//         <section className="card-body">
+//             <p>{props.body}</p>
+//         </section>
+//         <footer>
+//             <p>Card footer</p>
+//         </footer>
+//     </div>
+// )
+// }
 
 export default Card;
